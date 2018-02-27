5e50c180ffee09f5e7ff43392f12d1f1a36aed3a
Takeshi Ishita <ishitah.takeshi@gmail.com>
2018-02-27 06:59:55 +0000

    Add a script for setting environment

---
+++ setting-environment.sh
@@ -0,0 +1,10 @@
+apt update; apt upgrade; apt install sudo git vim zsh python3 zip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+sudo apt install python3-dev
+pip3 install cupy chainer chainercv
+pip3 install matplotlib
+apt install python3-tk
+jupyter notebook --ip 0.0.0.0 --port 9999 --allow-root
+apt install libglib-dev libglib2.0-dev
+pip3 install opencv-python
