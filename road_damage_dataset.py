5d189f1e8f2b36cd30fa9569001c616be3378bd6
Takeshi Ishita <ishitah.takeshi@gmail.com>
2018-02-27 06:53:10 +0000

    Add a trainer and a dataset generator along with the LICENSE file. The LICENSE file is also added since these scripts are based on the ones in ChainerCV

---
+++ road_damage_dataset.py
@@ -0,0 +1,112 @@
+import numpy as np
+import os
+import warnings
+import xml.etree.ElementTree as ET
+
+import chainer
+
+from chainercv.datasets.voc import voc_utils
+from chainercv.utils import read_image
+
+
+roaddamage_label_names = ['D00', 'D01', 'D10', 'D11', 'D20', 'D40', 'D43', 'D44']
+
+
+class RoadDamageDataset(chainer.dataset.DatasetMixin):
+
+    """Bounding box dataset for RoadDamageDataset.
+
+    The index corresponds to each image.
+
+    When queried by an index, if :obj:`return_difficult == False`,
+    this dataset returns a corresponding
+    :obj:`img, bbox, label`, a tuple of an image, bounding boxes and labels.
+    This is the default behaviour.
+    If :obj:`return_difficult == True`, this dataset returns corresponding
+    :obj:`img, bbox, label, difficult`. :obj:`difficult` is a boolean array
+    that indicates whether bounding boxes are labeled as difficult or not.
+
+    The bounding boxes are packed into a two dimensional tensor of shape
+    :math:`(R, 4)`, where :math:`R` is the number of bounding boxes in
+    the image. The second axis represents attributes of the bounding box.
+    They are :math:`(y_{min}, x_{min}, y_{max}, x_{max})`, where the
+    four attributes are coordinates of the top left and the bottom right
+    vertices.
+
+    The labels are packed into a one dimensional tensor of shape :math:`(R,)`.
+    :math:`R` is the number of bounding boxes in the image.
+    The class name of the label :math:`l` is :math:`l` th element of
+    :obj:`roadddamage_label_names`.
+
+    The array :obj:`difficult` is a one dimensional boolean array of shape
+    :math:`(R,)`. :math:`R` is the number of bounding boxes in the image.
+    If :obj:`use_difficult` is :obj:`False`, this array is
+    a boolean array with all :obj:`False`.
+
+    The type of the image, the bounding boxes and the labels are as follows.
+
+    * :obj:`img.dtype == numpy.float32`
+    * :obj:`bbox.dtype == numpy.float32`
+    * :obj:`label.dtype == numpy.int32`
+    * :obj:`difficult.dtype == numpy.bool`
+
+    Args:
+        data_dir (string): Path to the root of the training data. If this is
+            :obj:`auto`, this class will automatically download data for you
+            under :obj:`$CHAINER_DATASET_ROOT/pfnet/chainercv/voc`.
+        split ({'train', 'val', 'trainval', 'test'}): Select a split of the
+            dataset. :obj:`test` split is only available for
+            2007 dataset.
+    """
+
+    def __init__(self, data_dir, split='train'):
+
+        if split not in ['train', 'trainval', 'val']:
+            raise ValueError(
+                "split must be either of 'train', 'traival', or 'val'"
+            )
+
+        id_list_file = os.path.join(
+            data_dir, 'ImageSets/Main/{0}.txt'.format(split))
+
+        self.ids = [id_.strip() for id_ in open(id_list_file)]
+
+        self.data_dir = data_dir
+
+    def __len__(self):
+        return len(self.ids)
+
+    def get_example(self, i):
+        """Returns the i-th example.
+
+        Returns a color image and bounding boxes. The image is in CHW format.
+        The returned image is RGB.
+
+        Args:
+            i (int): The index of the example.
+
+        Returns:
+            tuple of an image and bounding boxes
+
+        """
+        id_ = self.ids[i]
+        anno = ET.parse(
+            os.path.join(self.data_dir, 'Annotations', id_ + '.xml'))
+        bbox = []
+        label = []
+        for obj in anno.findall('object'):
+            bndbox_anno = obj.find('bndbox')
+
+            # subtract 1 to make pixel indexes 0-based
+            bbox.append([
+                int(bndbox_anno.find(tag).text) - 1
+                for tag in ('ymin', 'xmin', 'ymax', 'xmax')])
+            name = obj.find('name').text.strip()
+            label.append(roaddamage_label_names.index(name))
+        bbox = np.stack(bbox).astype(np.float32)
+        label = np.stack(label).astype(np.int32)
+
+        # Load an image
+        img_file = os.path.join(self.data_dir, 'JPEGImages', id_ + '.jpg')
+        img = read_image(img_file, color=True)
+        return img, bbox, label
