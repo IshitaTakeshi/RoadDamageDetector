f070418594de4f6348d5ebcca92e2fd7061854a3
Takeshi Ishita <ishitah.takeshi@gmail.com>
2018-03-02 12:10:01 +0000

    Add a demo script

---
+++ demo.py
@@ -0,0 +1,47 @@
+import argparse
+import matplotlib.pyplot as plot
+
+import chainer
+from chainer.serializers import load_npz
+
+from chainercv.links import SSD300
+from chainercv.links import SSD512
+from chainercv import utils
+from chainercv.visualizations import vis_bbox
+
+from utils import roaddamage_label_names
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model', choices=('ssd300', 'ssd512'), default='ssd300')
+    parser.add_argument('--gpu', type=int, default=-1)
+    parser.add_argument('--pretrained_model', required=True)
+    parser.add_argument('image')
+    args = parser.parse_args()
+
+    if args.model == 'ssd300':
+        model = SSD300(
+            n_fg_class=len(roaddamage_label_names),
+            pretrained_model=args.pretrained_model)
+    elif args.model == 'ssd512':
+        model = SSD512(
+            n_fg_class=len(roaddamage_label_names),
+            pretrained_model=args.pretrained_model)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()
+
+    img = utils.read_image(args.image, color=True)
+    bboxes, labels, scores = model.predict([img])
+    bbox, label, score = bboxes[0], labels[0], scores[0]
+
+    vis_bbox(
+        img, bbox, label, score, label_names=roaddamage_label_names)
+    plot.show()
+
+
+if __name__ == '__main__':
+    main()
