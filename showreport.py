cec43971f63e1220c902c656b4f297ed94e6898d
Takeshi Ishita <ishitah.takeshi@gmail.com>
2018-03-04 07:47:36 +0000

    Add a script to visualize training logs

---
+++ showreport.py
@@ -0,0 +1,72 @@
+import sys
+import json
+import random
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from pylab import rcParams
+
+rcParams['figure.figsize'] = 16, 20
+rcParams['figure.dpi'] = 240
+
+
+linestyles = ['-', '--', ':']
+
+
+logfile_path = sys.argv[1]
+with open(logfile_path, "r") as f:
+    logs = json.load(f)
+
+
+def extract(logs, keys):
+    def _extract(log):
+        items = []
+        for key in keys:
+            try:
+                item = log[key]
+            except:
+                item = None
+            items.append(item)
+        return items
+    L = list(zip(*[_extract(log) for log in logs]))
+    return np.array(L, dtype=np.double)
+
+
+iteration, loss = extract(logs, keys=[
+    "iteration",
+    "main/loss",
+])
+ax1 = plt.subplot(211)
+
+plt.ylabel("loss")
+plt.plot(iteration, loss)
+
+L = extract(logs, keys=[
+    "iteration",
+    'validation/main/ap/D00',
+    'validation/main/ap/D01',
+    'validation/main/ap/D10',
+    'validation/main/ap/D11',
+    'validation/main/ap/D20',
+    'validation/main/ap/D30',
+    'validation/main/ap/D40',
+    'validation/main/ap/D43',
+    'validation/main/ap/D44',
+    'validation/main/map'
+])
+
+labels = ['D00', 'D01', 'D10', 'D11', 'D20', 'D30', 'D40', 'D43', 'D44', 'mAP']
+
+iteration, aps = L[0], L[1:]
+
+plt.subplot(212, sharex=ax1)
+plt.ylim([0, 1])
+for ap, label in zip(aps, labels):
+    masks = np.logical_not(np.isnan(ap))
+    plt.plot(iteration[masks], ap[masks], linestyle=random.choice(linestyles), label=label)
+
+plt.xlabel("iteration")
+plt.legend()
+
+plt.show()
