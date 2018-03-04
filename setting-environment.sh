ae1367723875e89a3b7955cc3db3bfe78f760f30
Takeshi Ishita <ishitah.takeshi@gmail.com>
2018-03-04 09:39:12 +0000

    Automatically remove get-pip.py after installing pip

--- setting-environment.sh
+++ setting-environment.sh
@@ -1,6 +1,7 @@
 apt update; apt upgrade -y; apt install -y sudo git vim zsh python3 zip
 wget -c https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
+rm get-pip.py
 apt install -y python3-dev
 pip3 install cupy chainer chainercv
 apt install -y python3-tk libglib2.0-0 libsm-dev
