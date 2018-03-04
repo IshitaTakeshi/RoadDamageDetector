7c2ee36a26147ec40439ab21146932da957d8e0e
Takeshi Ishita <ishitah.takeshi@gmail.com>
2018-03-04 09:37:06 +0000

    Append -y option to apt. Automatically create the merged dataset on setting environment

--- setting-environment.sh
+++ setting-environment.sh
@@ -1,9 +1,10 @@
-apt update; apt upgrade; apt install sudo git vim zsh python3 zip
-wget https://bootstrap.pypa.io/get-pip.py
+apt update; apt upgrade -y; apt install -y sudo git vim zsh python3 zip
+wget -c https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-apt install python3-dev
+apt install -y python3-dev
 pip3 install cupy chainer chainercv
-apt install python3-tk libglib2.0-0 libsm-dev
+apt install -y python3-tk libglib2.0-0 libsm-dev
 pip3 install matplotlib opencv-python
 wget -c https://s3-ap-northeast-1.amazonaws.com/mycityreport/RoadDamageDataset.tar.gz
 tar xvf RoadDamageDataset.tar.gz
+./merge_dasatet.sh
