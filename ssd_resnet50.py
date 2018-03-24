0ac3d1b0314149e370df5a878a5751d719f24501
Ishita Takeshi <ishitah.takeshi@gmail.com>
2018-03-24 11:40:17 +0000

    Experiment with ResNet-50

--- ssd_resnet50.py
+++ ssd_resnet50.py
@@ -1,16 +1,16 @@
 import chainer
-from chainer.links import Linear, ResNet101Layers
+from chainer.links import Linear, ResNet50Layers
 from chainer import functions as F
 from chainercv.links.model.ssd.ssd_vgg16 import (_load_npz, _imagenet_mean)
 from chainercv.links.model.ssd import Multibox, SSD
 
 
-class ResNet101FineTuning(chainer.Chain):
+class ResNet50FineTuning(chainer.Chain):
     def __init__(self, n_class, pretrained_model='auto'):
-        super(ResNet101FineTuning, self).__init__()
+        super(ResNet50FineTuning, self).__init__()
 
         with self.init_scope():
-            self.base = ResNet101Layers(pretrained_model)
+            self.base = ResNet50Layers(pretrained_model)
             self.fc6 = Linear(2048, n_class)
 
     def __call__(self, x):
@@ -19,16 +19,16 @@ class ResNet101FineTuning(chainer.Chain):
         return F.softmax(self.fc6(h))
 
 
-class ResNet101Extractor(ResNet101Layers):
+class ResNet50Extractor(ResNet50Layers):
     insize = 224
     grids = (56, 28, 14, 7)
 
     def __init__(self, pretrained_model='auto'):
-        super(ResNet101Extractor, self).__init__(pretrained_model)
+        super(ResNet50Extractor, self).__init__(pretrained_model)
 
     def __call__(self, x):
         layers = ["res2", "res3", "res4", "res5"]
-        activations = super(ResNet101Extractor, self).__call__(x, layers)
+        activations = super(ResNet50Extractor, self).__call__(x, layers)
         return [activations[layer] for layer in layers]
 
 
@@ -36,7 +36,7 @@ class SSD224(SSD):
     """Single Shot Multibox Detector with 224x224 inputs.
 
     This is a model of Single Shot Multibox Detector [#]_.
-    This model uses :class:`ResNet101Extractor` as its feature extractor.
+    This model uses :class:`ResNet50Extractor` as its feature extractor.
 
     .. [#] Wei Liu, Dragomir Anguelov, Dumitru Erhan, Christian Szegedy,
        Scott Reed, Cheng-Yang Fu, Alexander C. Berg.
@@ -49,7 +49,7 @@ class SSD224(SSD):
             * `filepath`: A path of npz file. In this case, :obj:`n_fg_class` \
                 must be specified properly.
             * :obj:`None`: Do not load weights.
-       pretrained_extractor (str): The `npz` weight file of `ResNet101Layers`.
+       pretrained_extractor (str): The `npz` weight file of `ResNet50Layers`.
            If this argument is specified as `auto`, it automatically loads and
            converts the caffemodel.
     """
@@ -59,7 +59,7 @@ class SSD224(SSD):
                  pretrained_model=None):
 
         super(SSD224, self).__init__(
-            extractor=ResNet101Extractor(pretrained_extractor),
+            extractor=ResNet50Extractor(pretrained_extractor),
             multibox=Multibox(
                 n_class=n_fg_class + 1,
                 aspect_ratios=((2, 3), (2, 3), (2, 3), (2, 3))),
