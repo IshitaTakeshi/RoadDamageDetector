ce4c69d44711b9f3e93eb5c3944617a8e1a9d8df
Ishita Takeshi <ishitah.takeshi@gmail.com>
2018-04-05 17:31:35 +0000

    Make the base network specifiable

--- demo.py
+++ demo.py
@@ -4,23 +4,35 @@ from matplotlib import pyplot as plt
 import chainer
 from chainer.serializers import load_npz
 
-from ssd_resnet101 import SSD224
+import chainercv
 from chainercv import utils
 from chainercv.visualizations import vis_bbox
 
+import ssd_resnet101
 from utils import roaddamage_label_names
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
-    parser.add_argument('--pretrained_model', required=True)
+    parser.add_argument('--base-network', choices=('vgg16', 'resnet101'),
+                        default='vgg16', help='Base network')
+    parser.add_argument('--pretrained-model', required=True)
     parser.add_argument('image')
     args = parser.parse_args()
 
-    model = SSD224(
-        n_fg_class=len(roaddamage_label_names),
-        pretrained_model=args.pretrained_model)
+    if args.base_network == 'vgg16':
+        # pretrained_extractor is currently not available for this class
+        model = chainercv.links.SSD300(
+           n_fg_class=len(roaddamage_label_names),
+           pretrained_model=args.pretrained_model)
+    elif args.base_network == 'resnet101':
+        model = ssd_resnet101.SSD224(
+           n_fg_class=len(roaddamage_label_names),
+           pretrained_extractor=args.pretrained_extractor,
+           pretrained_model=args.pretrained_model)
+    else:
+        raise ValueError('Invalid base network')
 
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
